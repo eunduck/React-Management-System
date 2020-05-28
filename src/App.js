b96865fc3cbda5990cd6086e7d013774dd3f8b12
eunduck <kimisduck@gmail.com>
2020-05-28 13:57:17 +0900

    Table, CSS 적용

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
 import Customer from './components/Customer';
 import './App.css';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import { TableHead, TableBody, TableRow, TableCell } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+});
 
 const customers = [
   {
@@ -29,23 +44,38 @@ const customers = [
   }
 ];
 
-class App extends Component{
+class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        {customers.map(c => {
-          return <Customer
-          id={c.id}
-          image={c.image}
-          name={c.name}
-          age={c.age}
-          job={c.job}
-          citu={c.city}
-          />
-        })}
-      </div>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>나이</TableCell>
+              <TableCell>직업</TableCell>
+              <TableCell>지역</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map(c => {
+              return <Customer key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                age={c.age}
+                job={c.job}
+                citu={c.city}
+              />
+            })}
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
