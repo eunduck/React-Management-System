00276eab59bd9430960cd6f708a13f5345657228
eunduck <kimisduck@gmail.com>
2020-06-02 09:44:52 +0900

    Revert "Revert "json 수정""
    This reverts commit 4b76c1377b164420b263978ea42df3cc313b3f89.

--- server.js
+++ server.js
@@ -6,7 +6,32 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true }));
 
 app.get('/api/hello', (req, res) => {
-    res.send({ message: 'Hello Express!' });
+    res.send([
+        {
+            'id': 1,
+            'image': 'https://placeimg.com/64/64/1',
+            'name': '김은덕1',
+            'age': '10대',
+            'job': '중학생',
+            'city': '서울'
+          },
+          {
+            'id': 2,
+            'image': 'https://placeimg.com/64/64/2',
+            'name': '김은덕2',
+            'age': '20대',
+            'job': '입사자',
+            'city': '서울'
+          },
+          {
+            'id': 3,
+            'image': 'https://placeimg.com/64/64/3',
+            'name': '김은덕3',
+            'age': '30대',
+            'job': '직장인',
+            'city': '서울'
+          }
+    ]);
 });
 
 app.listen(port, () => console.log(`Listening on PORT ${port}`));
